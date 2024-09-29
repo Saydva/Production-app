5f742cc651ece3c646d2372ea5ec00af39ab01f3
Saydva <kacurikjozef@gmail.com>
2024-09-29 19:00:27 +0200

    finished rest api all crud operations

--- server/routes/router.js
+++ server/routes/router.js
@@ -11,8 +11,13 @@ router.get("/", async (req, res) => {
     res.status(500).json({ message: error.message });
   }
 });
+
 //get one
-router.get("/:id", async (req, res) => {});
+router.get("/:id", getDataId, (req, res) => {
+  res.send(res.data.name);
+  console.log(res.data.name);
+});
+
 //create one
 router.post("/", async (req, res) => {
   const data = new Data({
@@ -26,9 +31,47 @@ router.post("/", async (req, res) => {
     res.status(400).json({ message: error.message });
   }
 });
+
 //update
-router.patch("/", (req, res) => {});
+router.patch("/:id", getDataId, async (req, res) => {
+  if (req.body.name != null) {
+    res.data.name = req.body.name;
+  }
+  if (req.body.dataNumber != null) {
+    res.data.dataNumber = req.body.dataNumber;
+  }
+  try {
+    const newData = await res.data.save();
+    res.json(newData);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+});
+
 //delete
-router.delete("/:id", (req, res) => {});
+router.delete("/:id", getDataId, async (req, res) => {
+  try {
+    await res.data.deleteOne();
+    res.status(201).json({ message: "Data removed" });
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+});
+
+//async middleware function to get data by id
+
+async function getDataId(req, res, next) {
+  let data;
+  try {
+    data = await Data.findById(req.params.id);
+    if (data == null) {
+      return res.status(404).send({ message: "Can not find data" });
+    }
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+  res.data = data;
+  next();
+}
 
 module.exports = router;
