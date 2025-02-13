b5e462598992daec5de7d2927a281a22748e5b20
Saydva <kacurikjozef@gmail.com>
2025-02-13 08:24:54 +0100

    selectRowComponent finished

--- client/src/components/buildComponents/selectRowComponent.jsx
+++ client/src/components/buildComponents/selectRowComponent.jsx
@@ -1,4 +1,5 @@
 import { React, useEffect, useState } from "react";
+import { XCircle } from "react-feather";
 
 function SelectComponent() {
   const array = [
@@ -7,36 +8,46 @@ function SelectComponent() {
     { name: "three", set: 3 },
   ];
 
-  const options = array.map((e, index) => {
-    return (
-      <option value={JSON.stringify(e)} key={index}>
-        {e.name}
-      </option>
-    );
-  });
-
   const [lock, setLock] = useState(false);
   const [data, setData] = useState([]);
+
+  let options;
+
+  if (!lock) {
+    options = array.map((e, index) => {
+      return (
+        <option
+          className="m-2 rounded-full"
+          value={JSON.stringify(e)}
+          key={index}
+        >
+          {e.name}
+        </option>
+      );
+    });
+  }
 
   const remove = (i) => {
     data.splice(i, 1);
     setData([...data]);
   };
 
-  console.log(data);
-
   const list = data.map((e, index) => (
-    <li id={index} key={index}>
-      {e}
+    <div role="alert" className="alert alert-success ml-3 mb-3 p-2 flex">
       <button
         id={index}
         onClick={(e) => {
-          remove(e.target.id);
+          if (!lock) {
+            remove(e.target.id);
+          }
         }}
       >
-        x
+        <XCircle />
       </button>
-    </li>
+      <span id={index} key={index}>
+        {e}
+      </span>
+    </div>
   ));
 
   let select;
@@ -53,18 +64,44 @@ function SelectComponent() {
 
   return (
     <div className="flex flex-row items-center">
-      <div
-        className={`w-full shadow-md shadow-slate-500 motion-preset-slide-right m-3 max-w-xs
-          ${lock ? "text-error focus:text-error" : ""}`}
-      >
-        <select onChange={onChange} className="select select-secondary w-full ">
-          <option value="">Pick something</option>
+      <div>
+        <select
+          disabled={lock}
+          onChange={onChange}
+          className={`input input-ghost w-full max-w-xs shadow-md shadow-slate-500 border-neutral border-2 motion-preset-slide-right m-3 min-w-56 $
+          lock ? "text-error focus:text-error" : ""
+        }`}
+        >
+          <option value="">{!lock ? "Pick Something" : "Locked"}</option>
           {options}
         </select>
-        <ul>
-          <p>hi</p>
-          {list}
-        </ul>
+        <ul>{list}</ul>
+
+        <button
+          className="btn ml-3"
+          onClick={(e) => {
+            // set data to value
+
+            // add clases to amine button
+            e.target.classList.add(
+              ["motion-preset-shrink"],
+              ["bg-info"],
+              ["hover:bg-info"],
+              ["text-slate-900"]
+            );
+            setTimeout(() => {
+              e.target.classList.remove(
+                ["motion-preset-shrink"],
+                ["bg-info"],
+                ["hover:bg-info"],
+                ["text-slate-900"]
+              );
+            }, 1000);
+            setLock(!lock);
+          }}
+        >
+          {lock ? "Unlock" : "Lock"}
+        </button>
       </div>
     </div>
   );
